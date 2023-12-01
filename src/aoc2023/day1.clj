ffc79d45ed2ff773ba0feefd1ea1441a4ec38274
Josh Kingsley <josh@joshkingsley.me>
2023-12-01 17:15:16 +0200

    Solve day 1 part 2

--- src/aoc2023/day1.clj
+++ src/aoc2023/day1.clj
@@ -5,11 +5,39 @@
 
 (def input (slurp (io/resource "aoc2023/day1_input.txt")))
 
-(def first-and-last-numbers-re #"^[^\d]*(\d)(?:[^\d]|\d(?=[^\d]*\d))*(\d)?.*$")
+(def number-re #"(?:\d|one|two|three|four|five|six|seven|eight|nine)")
+
+(def first-and-last-numbers-re
+  (re-pattern
+   (string/replace
+    (str #"^(?:(?!NUMBER).)*(NUMBER)(?:(?!NUMBER).|(?=NUMBER).(?=(?:(?!NUMBER).)*NUMBER))*(NUMBER)?.*$")
+    "NUMBER"
+    (str number-re))))
+
+(def numbers
+  {"0" 0
+   "1" 1
+   "2" 2
+   "3" 3
+   "4" 4
+   "5" 5
+   "6" 6
+   "7" 7
+   "8" 8
+   "9" 9
+   "one" 1
+   "two" 2
+   "three" 3
+   "four" 4
+   "five" 5
+   "six" 6
+   "seven" 7
+   "eight" 8
+   "nine" 9})
 
 (defn parse-line [s]
   (let [[_ a b] (re-matches first-and-last-numbers-re s)]
-    (Long/parseUnsignedLong (str a (or b a)))))
+    (Long/parseUnsignedLong (str (numbers a) (numbers (or b a))))))
 
 (defn answer []
   (transduce (map parse-line) + 0 (string/split-lines input)))

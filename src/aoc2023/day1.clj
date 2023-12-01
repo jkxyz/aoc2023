44a07d103f53437fc02ef619f7ee562f1fb9c960
Josh Kingsley <josh@joshkingsley.me>
2023-12-01 16:39:13 +0200

    Solve day 1 part 1

--- src/aoc2023/day1.clj
+++ src/aoc2023/day1.clj
@@ -1,2 +1,18 @@
-(ns aoc2023.day1)
+(ns aoc2023.day1
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as string]))
 
+(def input (slurp (io/resource "aoc2023/day1_input.txt")))
+
+(def first-and-last-numbers-re #"^[^\d]*(\d)(?:[^\d]|\d(?=[^\d]*\d))*(\d)?.*$")
+
+(defn parse-line [s]
+  (let [[_ a b] (re-matches first-and-last-numbers-re s)]
+    (Long/parseUnsignedLong (str a (or b a)))))
+
+(defn answer []
+  (transduce (map parse-line) + 0 (string/split-lines input)))
+
+(comment
+  (answer))
